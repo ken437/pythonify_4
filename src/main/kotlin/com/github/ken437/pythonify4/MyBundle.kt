e3f2ff948b172d7744a43cfbc8b1c8c8b5820de1
GitHub Action <action@github.com>
2021-10-15 17:49:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/ken437/pythonify4/MyBundle.kt
+++ src/main/kotlin/com/github/ken437/pythonify4/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ken437.pythonify4
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ken437/pythonify4/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ken437/pythonify4/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ken437.pythonify4.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ken437.pythonify4.MyBundle
 
 class MyApplicationService {
 
